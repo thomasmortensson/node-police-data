e85ed33d034c685cce512a66e0967b30abb3cbb4
Thomas Mortensson <thomasmortensson@gmail.com>
2015-06-08 15:22:32 +0100

    Click to get reports

--- www/js/leaflet.js
+++ www/js/leaflet.js
@@ -1,13 +1,15 @@
 // create a map in the "map" div, set the view to a given place and zoom
-var map = L.map('map').setView([52.629729, -2.609690], 13);
+var map = L.map('map').setView([51.454513, -2.587910], 13);
 
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// add a marker in the given location, attach some popup content to it and open the popup
-$.get("api/police?lat=52.629729&lng=-2.609690&year=2015&month=03", function(data, status){
+
+function onMapClick(e) {
+    console.log(e.latlng);
+    $.get("api/police?lat=" + e.latlng.lat + "&lng=" + e.latlng.lng+ "&year=2015&month=03", function(data, status){
     var police_data = JSON.parse(data);
     for (var i=0; i<police_data.length; i++) {
     	console.log(police_data[i].category);
@@ -17,4 +19,9 @@ $.get("api/police?lat=52.629729&lng=-2.609690&year=2015&month=03", function(data
     }	
 
 });
+}
+
+map.on('click', onMapClick);
+// add a marker in the given location, attach some popup content to it and open the popup
+//
 
