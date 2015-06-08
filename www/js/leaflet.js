25548a784dcc7925a3fcba05bb7df61028de9735
Thomas Mortensson <thomasmortensson@gmail.com>
2015-06-08 14:37:27 +0100

    Added some markers

--- www/js/leaflet.js
+++ www/js/leaflet.js
@@ -1,5 +1,5 @@
 // create a map in the "map" div, set the view to a given place and zoom
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([52.629729, -2.609690], 13);
 
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -7,11 +7,14 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // add a marker in the given location, attach some popup content to it and open the popup
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
-    .openPopup();
-
 $.get("api/police?lat=52.629729&lng=-2.609690&year=2015&month=03", function(data, status){
-    console.log(data);
+    var police_data = JSON.parse(data);
+    for (var i=0; i<police_data.length; i++) {
+    	console.log(police_data[i].category);
+    	L.marker([police_data[i].location.latitude, police_data[i].location.longitude]).addTo(map)
+			.bindPopup(police_data[i].category)
+			.openPopup();
+    }	
+
 });
 
